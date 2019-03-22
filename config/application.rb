bd9cd2eca9f0a6246a6033a303da54d29eae6d0f
mistermur <brian.j.murillo@gmail.com>
2019-03-22 16:18:22 -0400

    rails new api postgresql

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,13 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RideShareApi
+module RideshareApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "RideShare_api_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "rideshare-api_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
