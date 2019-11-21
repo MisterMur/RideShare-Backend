c43a46459ba2a6b18b79320a2d4cc1b84cacdd09
MisterMur <brian.j.murillo@gmail.com>
2019-11-21 02:09:06 -0500

    initial followers commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   has_many :messages
   has_many :forums, through: :messages
   has_many :rides
-  
-has_many :follower_follows, foreign_key: :followee_id, class_name: "Friendship"
-  has_many :followers, through: :follower_follows, source: :follower
 
-has_many :followee_follows, foreign_key: :follower_id, class_name: "Friendship"
-  has_many :followees, through: :followee_follows, source: :folowee
+# has_many :follower_follows, foreign_key: :followee_id, class_name: "Friendship"
+#   has_many :followers, through: :follower_follows, source: :follower
+has_many :active_relationships, class_name: "Friendship", foreign_key: "follower_id", dependent: :destroy
+has_many :passive_relationships, class_name: "Friendship", foreign_key: "followee_id", dependent: :destroy
+
+has_many :followers, through: :active_relationships, source: :followed_user
+has_many :followees, through: :passive_relationships, source: :follower_user
+# has_many :followee_follows, foreign_key: :follower_id, class_name: "Friendship"
+#   has_many :followees, through: :followee_follows, source: :folowee
 
 end
